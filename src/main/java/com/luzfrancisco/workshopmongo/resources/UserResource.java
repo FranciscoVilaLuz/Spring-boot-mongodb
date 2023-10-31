f19c61b7eb439e17760596e69050efbb49f9468c
Francisco Luz <franciscopato@hotmail.com>
2023-10-31 20:32:42 +0000

    Getting a user by ID

--- src/main/java/com/luzfrancisco/workshopmongo/resources/UserResource.java
+++ src/main/java/com/luzfrancisco/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.luzfrancisco.workshopmongo.domain.User;
 import com.luzfrancisco.workshopmongo.dto.UserDTO;
 import com.luzfrancisco.workshopmongo.services.UserService;
+
 
 @RestController
 @RequestMapping(value="/users")
@@ -26,5 +28,11 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+ 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
