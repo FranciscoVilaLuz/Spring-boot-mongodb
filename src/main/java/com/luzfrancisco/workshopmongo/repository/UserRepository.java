de29d75cc812df43ad76e632875b89169f131e3d
Francisco Luz <franciscopato@hotmail.com>
2023-10-31 19:40:06 +0000

    Connecting to MongoDB with Repository and Service

---
+++ src/main/java/com/luzfrancisco/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.luzfrancisco.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luzfrancisco.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
