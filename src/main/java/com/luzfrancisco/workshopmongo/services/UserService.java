c931b7a0a14c096eb511349adfd9a56472258fcd
Francisco Luz <franciscopato@hotmail.com>
2023-10-31 20:43:28 +0000

    User isertion with post

--- src/main/java/com/luzfrancisco/workshopmongo/services/UserService.java
+++ src/main/java/com/luzfrancisco/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luzfrancisco.workshopmongo.domain.User;
+import com.luzfrancisco.workshopmongo.dto.UserDTO;
 import com.luzfrancisco.workshopmongo.repository.UserRepository;
 import com.luzfrancisco.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -15,13 +16,21 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
